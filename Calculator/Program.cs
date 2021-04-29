a21092ddbd02ead95721ffe534422dbd6a6f4508
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-04-29 18:57:18 -0500

    started a new project

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someText = "Here is some text";
+            int number = 10;
+            double anotherNumber = 10.9;
+            bool isOrdered = true;
+            string input = Console.ReadLine();
+            var anything = 10.5;
+
+            Console.WriteLine(someText);
+            Console.WriteLine(number);
+            Console.WriteLine(anotherNumber);
+            Console.WriteLine(isOrdered);
+            Console.WriteLine(input);
+            Console.WriteLine(anything);
+
+        }
+    }
+}
